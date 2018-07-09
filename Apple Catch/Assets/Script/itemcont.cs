1fdd596fb489b1495213d2f195b1d606a8d9dcc0
Horiuchi Fubuki <fubuki199914@gmail.com>
2018-07-09 18:18:21 +0900

    バスケット

---
+++ Apple Catch/Assets/Script/itemcont.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemcont : MonoBehaviour {
+
+    public float dropspeed = -0.3f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, this.dropspeed, 0);
+        if(transform.position.y < -1.0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
