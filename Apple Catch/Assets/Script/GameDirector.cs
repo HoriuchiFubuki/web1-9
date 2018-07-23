74abdb046c2671962349ca5fb243bf82f993c645
Horiuchi Fubuki <fubuki199914@gmail.com>
2018-07-23 18:14:01 +0900

    テレポーテーション

--- Apple Catch/Assets/Script/GameDirector.cs
+++ Apple Catch/Assets/Script/GameDirector.cs
@@ -7,10 +7,13 @@ public class GameDirector : MonoBehaviour {
 
     GameObject timerText;
     GameObject pointText;
-    float time = 60.0f;
+    GameObject generator;
+
+    float time = 30.0f;
     int point = 0;
 	// Use this for initialization
 	void Start () {
+        this.generator = GameObject.Find("ItemGenerator");
         this.timerText = GameObject.Find("Time");
         this.pointText = GameObject.Find("Point");
 	}
@@ -18,11 +21,34 @@ public class GameDirector : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         this.time -= Time.deltaTime;
+
+        if(this.time < 0)
+        {
+            this.time = 0;
+            this.generator.GetComponent<itemGenerator>().SetParameter(10000.0f, 0, 0);
+        }else if(0 <= this.time && this.time < 5)
+        {
+            this.generator.GetComponent<itemGenerator>().SetParameter(0.9f, -0.04f, 3);
+        }
+        else if (5 <= this.time && this.time < 10)
+        {
+            this.generator.GetComponent<itemGenerator>().SetParameter(0.5f, -0.05f, 6);
+        }
+        else if (10 <= this.time && this.time < 20)
+        {
+            this.generator.GetComponent<itemGenerator>().SetParameter(0.8f, -0.04f, 4);
+        }
+        else if (20 <= this.time && this.time < 30)
+        {
+            this.generator.GetComponent<itemGenerator>().SetParameter(1.0f, -0.03f, 2);
+        }
+
         this.timerText.GetComponent<Text>().text =
             this.time.ToString("F1");
         this.pointText.GetComponent<Text>().text =
             this.point.ToString() + " point";
 	}
+
     public void GetApple()
     {
         this.point += 100;
--- Apple Catch/Assets/Script/itemcont.cs
+++ Apple Catch/Assets/Script/itemcont.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class itemcont : MonoBehaviour {
 
     public float dropspeed = -0.3f;
+    int randPos;
+    bool onechan = true;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,6 +15,43 @@ public class itemcont : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.Translate(0, this.dropspeed, 0);
+        if (transform.position.y <= 1.5f)
+        {
+            if(onechan == true){
+                randPos = Random.Range(1, 9);
+                switch (randPos) {
+                    case 1:
+                        transform.position = new Vector3(-1.0f, transform.position.y, 1.0f);
+                        break;
+                    case 2:
+                        transform.position = new Vector3(0, transform.position.y, 1.0f);
+                        break;
+                    case 3:
+                        transform.position = new Vector3(1.0f, transform.position.y, 1.0f);
+                        break;
+                    case 4:
+                        transform.position = new Vector3(-1.0f, transform.position.y, 0);
+                        break;
+                    case 5:
+                        transform.position = new Vector3(0, transform.position.y, 0);
+                        break;
+                    case 6:
+                        transform.position = new Vector3(1.0f, transform.position.y, 0);
+                        break;
+                    case 7:
+                        transform.position = new Vector3(-1.0f, transform.position.y, -1.0f);
+                        break;
+                    case 8:
+                        transform.position = new Vector3(0, transform.position.y, -1.0f);
+                        break;
+                    case 9:
+                        transform.position = new Vector3(1.0f, transform.position.y, -1.0f);
+                        break;
+                }
+                onechan = false;
+                GetComponent<ParticleSystem>().Play();
+            }
+        }
         if(transform.position.y < -1.0f)
         {
             Destroy(gameObject);
